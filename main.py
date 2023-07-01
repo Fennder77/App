9c120d1ba09242bd1095d50e5282d1132c24f14c
Fennder77 <depman7777@gmail.com>
2023-07-01 23:18:34 +0300

    chenges comand font

--- main.py
+++ main.py
@@ -2,39 +2,38 @@ import customtkinter
 from tkinter import *
 from PIL import ImageTk
 
-customtkinter.set_appearance_mode("dark")
-customtkinter.set_default_color_theme("dark-yellow")
+customtkinter.set_appearance_mode("dark")#смена темы
+customtkinter.set_default_color_theme("dark-blue")#цвета виджетов
 
 
 def add(task):
     f = customtkinter.CTkFrame(root)
 
-    customtkinter.CTkCheckBox(f, text=task).pack(anchor=NW, side=LEFT)
-    customtkinter.CTkButton(f, image=img_del, text='', width=30, command=lambda: f.pack_forget()).pack(anchor=NW, side=LEFT, padx=10)
-    f.pack(anchor=NW, padx=5, pady=5)
+    customtkinter.CTkCheckBox(f, text=task).pack(anchor=NW, side=LEFT)#виджет
+    customtkinter.CTkButton(f, image=img_del, text='', width=30, command=lambda: f.pack_forget()).pack(anchor=NW, side=LEFT, padx=10)#конопка для удаления задачи
+    f.pack(anchor=NW, padx=5, pady=5)#отображение виджета
 
 
 def add_task():
-    window = customtkinter.CTkToplevel(root)
-    window.title('Добавить задачу')
-    window.geometry('300x80')
+    window = customtkinter.CTkToplevel(root)#дочернее окно
+    window.title('Добавить задачу')#заголовок
+    window.geometry('300x80')#разрешение
 
-    task_text = customtkinter.CTkEntry(window, width=250)
-    task_text.pack(pady=5)
+    task_text = customtkinter.CTkEntry(window, width=250)#текстовое поле
+    task_text.pack(pady=5)#отступ
 
-    customtkinter.CTkButton(window, text='Добавить', text_font='Arial 13 bold', command=lambda: add(task_text.get())).pack()
+    customtkinter.CTkButton(window, text='Добавить', font=('Times', 13), command=lambda: add(task_text.get())).pack()#кнопка на дочернее окно
 
     window.mainloop()
 
 
-root = customtkinter.CTk()
-root.title("Планировщик задач")
-root.geometry('700x300')
+root = customtkinter.CTk()#Создание класса
+root.title("Планировщик задач")#заголовок
+root.geometry('700x300')#разрешение окна
 
-img_del = ImageTk.PhotoImage(file='del.png')
+img_del = ImageTk.PhotoImage(file='del.png')#отметка
 
-btn_add_task = customtkinter.CTkButton(root, text='Добавить задачу', text_font='Arial 13 bold', command=add_task)
-btn_add_task.pack(anchor=S, side=BOTTOM, pady=5)
+btn_add_task = customtkinter.CTkButton(root, text='Добавить задачу', font=('Times', 13), command=add_task)#добаавить кнопку
+btn_add_task.pack(anchor=S, side=BOTTOM, pady=5)#росположение кнопки
 
 root.mainloop()
-# App
